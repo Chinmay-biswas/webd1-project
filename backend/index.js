8d8cf7b40fa37a164923cc1125fe49f76cc39d4e
Chinmay-biswas <94024253+Chinmay-biswas@users.noreply.github.com>
2025-06-24 22:12:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,5 +44,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(🚀 Listening on http://localhost:${PORT})
+  console.log(Listening on http://localhost:${PORT})
 })
