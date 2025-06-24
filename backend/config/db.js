6194a855504eb169aa5c20612a79509e0136b5e2
laladwesh <guptaavinash302@gmail.com>
2025-06-24 19:13:09 +0530

    its fucking fixed chinmayy

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URI);
+    const con = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/yourdbname');
       console.log(`MongoDB connected : ${con.connection.host}`);
     } catch (error) {
     console.error(`Error: ${error.message}`);
